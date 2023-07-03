49302636adec5939457561bfd0985b9e8cd249b7
Eltrac <hi@guhub.cn>
2023-07-03 23:28:12 +0800

    try head

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -2,17 +2,9 @@
     export let data;
 </script>
   
-<div class="mt-10">
-  <header class="text-center py-10">
-    <h1 class="font-bold text-5xl">{ data.title }</h1>
-    <p class="my-2 text-gray-600">{data.date} · {data.tags}</p>
-  </header>
-  <article class="yue max-w-screen-md mb-10 mx-auto">
-    <svelte:component this={data.content} />
-  </article>
-  <section id="comment" class="max-w-screen-md mb-10 mx-auto">
-    <div class="giscus" />
-    <script src="https://giscus.app/client.js"
+<svelte:head>
+  <title>{data.title} - Isla</title>
+  <script src="https://giscus.app/client.js"
         data-repo="BigCoke233/isla-giscus"
         data-repo-id="R_kgDOJ3CBvg"
         data-category="Announcements"
@@ -27,6 +19,18 @@
         data-loading="lazy"
         crossorigin="anonymous"
         async>
-    </script>
+  </script>
+</svelte:head>
+
+<div class="mt-10">
+  <header class="text-center py-10">
+    <h1 class="font-bold text-5xl">{ data.title }</h1>
+    <p class="my-2 text-gray-600">{data.date} · {data.tags}</p>
+  </header>
+  <article class="yue max-w-screen-md mb-10 mx-auto">
+    <svelte:component this={data.content} />
+  </article>
+  <section id="comment" class="max-w-screen-md mb-10 mx-auto">
+    <div class="giscus">Giscus 出了一点问题，刷新试试？</div>
   </section>
 </div>
