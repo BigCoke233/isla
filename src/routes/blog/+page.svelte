cab1f17d9d87cf404d571144b766275419a5160c
Eltrac <hi@guhub.cn>
2023-07-09 11:49:15 +0800

    renew the post list

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     export let data;
-    import Title from '../Title.svelte'
+    import Title from '../Title.svelte';
+
+    let i = 0;
 </script>
 
 <svelte:head>
@@ -10,23 +12,38 @@
 <div class="p-3 my-20" id="blog">
     <Title des="Isla 上唯一的资料库">树洞图书馆</Title>
 
-    <ol class="my-10 md:grid md:grid-cols-3 md:gap-5">
+    <ol class="my-16 max-w-screen-md mb-10 mx-auto text-lg">
         {#each data.posts as post}
-            <li class="list-none py-4 transition text-lg">
-                <span> {post.meta.date}</span>
-                <a href={post.path} class="font-semibold block flex-grow">{post.meta.title}</a>
-                <em class="text-gray-400 text-sm">{post.meta.tags}</em>
+            <li class="flex gap-8">
+                <span class="post-date"> {post.meta.date}</span>
+                <a class="post-link" href={post.path}>{post.meta.title}</a>
+                <em class="post-tags">{post.meta.tags}</em>
             </li>
         {/each}
     </ol>
 </div>
 
 <style lang="scss">
+    @import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap');
     #blog {
         ol li {
-            border-bottom: 1px dashed #ccc;
-            &:hover {
-                border-color: #333
+            margin: 1rem 0;
+            width: 100%;
+            span, a, em {
+                display: block
+            }
+            span.post-date {
+                font-family: 'IBM Plex Mono', monospace;
+                width: 6rem;
+                color: #999
+            }
+            a.post-link {
+                flex-grow: 1;
+                font-weight: 600
+            }
+            em.post-tags {
+                color: #999;
+                font-size: 60%
             }
         }
     }
